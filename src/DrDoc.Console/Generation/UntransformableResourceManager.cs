20725701ff0d4dbb1d3a9f95cf1e7116f07142e4
James Gregory <james@jagregory.com>
2009-03-15 17:53:21 +0000

    Implemented the untrasformable resource manager

--- src/DrDoc.Console/Generation/UntransformableResourceManager.cs
+++ src/DrDoc.Console/Generation/UntransformableResourceManager.cs
@@ -1,4 +1,4 @@
-using DrDoc.Generation;
+using System.IO;
 
 namespace DrDoc.Generation
 {
@@ -6,7 +6,18 @@ namespace DrDoc.Generation
     {
         public void MoveResources(string templatePath, string outputPath)
         {
-            
+            foreach (var file in Directory.GetFiles(templatePath, "*", SearchOption.AllDirectories))
+            {
+                if (Path.GetExtension(file) == ".spark") continue;
+
+                var newPath = file.Replace(templatePath, outputPath);
+                var newDirectory = Path.GetDirectoryName(newPath);
+
+                if (!Directory.Exists(newDirectory))
+                    Directory.CreateDirectory(newDirectory);
+
+                File.Copy(file, newPath);
+            }
         }
     }
 }
