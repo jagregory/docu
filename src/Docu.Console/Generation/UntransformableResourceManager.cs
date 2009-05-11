0054748a19a7e0fc291df536ad9f6a480cd8ce92
joshuaflanagan <joshuaflanagan@gmail.com>
2009-05-12 03:16:53 +0800

    Fixed problem where files under hidden subdirectories were not being ignored during final copy to output.
    Signed-off-by: James Gregory <james@jagregory.com>

--- src/Docu.Console/Generation/UntransformableResourceManager.cs
+++ src/Docu.Console/Generation/UntransformableResourceManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 
 namespace Docu.Generation
@@ -6,21 +7,40 @@ namespace Docu.Generation
     {
         public void MoveResources(string templatePath, string outputPath)
         {
-            foreach (string file in Directory.GetFiles(templatePath, "*", SearchOption.AllDirectories))
+            foreach (var file in AllFilesUnder(templatePath))
             {
                 if (Path.GetExtension(file) == ".spark") continue;
-                if ( (File.GetAttributes(file) & FileAttributes.Hidden) == FileAttributes.Hidden) continue;
-                var containingDirectory = Path.GetDirectoryName(file);
-                if ( (File.GetAttributes(containingDirectory) & FileAttributes.Hidden) == FileAttributes.Hidden) continue;
+                if (IsHiddenPath(file)) continue;
 
-                string newPath = file.Replace(templatePath, outputPath);
-                string newDirectory = Path.GetDirectoryName(newPath);
+                var newPath = file.Replace(templatePath, outputPath);
+                var newDirectory = Path.GetDirectoryName(newPath);
 
                 if (!Directory.Exists(newDirectory))
+                {
                     Directory.CreateDirectory(newDirectory);
+                }
 
                 File.Copy(file, newPath, true);
             }
         }
+
+        public static IEnumerable<string> AllFilesUnder(string targetDirectory)
+        {
+            var fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (var fileName in fileEntries)
+                yield return fileName;
+
+            foreach (var subdirectory in Directory.GetDirectories(targetDirectory))
+            {
+                if (IsHiddenPath(subdirectory)) continue;
+                foreach (var fileName in AllFilesUnder(subdirectory)) yield return fileName;
+            }
+        }
+
+        private static bool IsHiddenPath(string path)
+        {
+            return ((File.GetAttributes(path) & FileAttributes.Hidden) == FileAttributes.Hidden);
+        }
     }
+
 }
