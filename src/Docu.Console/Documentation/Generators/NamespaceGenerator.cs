cc605f0402274761197c72d012cb721676383d81
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-02-10 18:19:59 +0100

    Provide a better error message if type has no namespace, (Possible in F#)

--- src/Docu.Console/Documentation/Generators/NamespaceGenerator.cs
+++ src/Docu.Console/Documentation/Generators/NamespaceGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Docu.Parsing.Model;
 
@@ -14,6 +15,11 @@ namespace Docu.Documentation.Generators
 
         public void Add(List<Namespace> namespaces, IDocumentationMember association)
         {
+            if (association.TargetType.Namespace == null)
+                throw new NullReferenceException(
+                    string.Format("There was no namespace found for {0}",
+                                  association.TargetType.AssemblyQualifiedName));
+
             var ns = Identifier.FromNamespace(association.TargetType.Namespace);
 
             if (!namespaces.Exists(x => x.IsIdentifiedBy(ns)))
