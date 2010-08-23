b5b69af58f8ffc5975dd9cf65223852c9a48f34f
James Gregory <james@jagregory.com>
2010-08-24 00:20:48 +0100

    Removed implicit line endings from tests

--- src/Docu.Tests/Documentation/DocumentModelGeneratorTests/Examples.cs
+++ src/Docu.Tests/Documentation/DocumentModelGeneratorTests/Examples.cs
@@ -16,15 +16,7 @@ namespace Docu.Tests.Documentation.DocumentModelGeneratorTests
             var members = new IDocumentationMember[]
             {
                 Type<Second>(@"<member name=""T:Example.Second"" />"),
-                Method<Second>(@"
-                <member name=""M:Example.Second.SecondMethod2(System.String,System.Int32)"">
-                  <example>
-                    void Something()
-                    {
-                      return;
-                    }
-                  </example>
-                </member>", x => x.SecondMethod2(null, 0))
+                Method<Second>("<member name=\"M:Example.Second.SecondMethod2(System.String,System.Int32)\"><example>\r\n                    void Something()\r\n                    {\r\n                      return;\r\n                    }\r\n                  </example></member>", x => x.SecondMethod2(null, 0))
             };
             var namespaces = model.Create(members);
             var method = namespaces.Single().Classes.Single().Methods.Single();
@@ -42,15 +34,7 @@ namespace Docu.Tests.Documentation.DocumentModelGeneratorTests
             var members = new IDocumentationMember[]
             {
                 Type<Second>(@"<member name=""T:Example.Second"" />"),
-                Property<Second>(@"
-                <member name=""P:Example.Second.SecondProperty"">
-                  <example>
-                    void Something()
-                    {
-                      return;
-                    }
-                  </example>
-                </member>", x => x.SecondProperty)
+                Property<Second>("<member name=\"P:Example.Second.SecondProperty\"><example>\r\n                    void Something()\r\n                    {\r\n                      return;\r\n                    }\r\n                  </example></member>", x => x.SecondProperty)
             };
             var namespaces = model.Create(members);
             var property = namespaces.Single().Classes.Single().Properties.Single();
@@ -68,15 +52,7 @@ namespace Docu.Tests.Documentation.DocumentModelGeneratorTests
             var members = new IDocumentationMember[]
             {
                 Type<Second>(@"<member name=""T:Example.Second"" />"),
-                Field<Second>(@"
-                <member name=""F:Example.Second.aField"">
-                  <example>
-                    void Something()
-                    {
-                      return;
-                    }
-                  </example>
-                </member>", x => x.aField)
+                Field<Second>("<member name=\"F:Example.Second.aField\"><example>\r\n                    void Something()\r\n                    {\r\n                      return;\r\n                    }\r\n                  </example></member>", x => x.aField)
             };
             var namespaces = model.Create(members);
             var field = namespaces.Single().Classes.Single().Fields.Single();
@@ -94,15 +70,7 @@ namespace Docu.Tests.Documentation.DocumentModelGeneratorTests
             var members = new IDocumentationMember[]
             {
                 Type<Second>(@"<member name=""T:Example.Second"" />"),
-                Event<Second>(@"
-                <member name=""E:Example.Second.AnEvent"">
-                  <example>
-                    void Something()
-                    {
-                      return;
-                    }
-                  </example>
-                </member>", "AnEvent")
+                Event<Second>("<member name=\"E:Example.Second.AnEvent\"><example>\r\n                    void Something()\r\n                    {\r\n                      return;\r\n                    }\r\n                  </example></member>", "AnEvent")
             };
             var namespaces = model.Create(members);
             var @event = namespaces.Single().Classes.Single().Events.Single();
@@ -119,14 +87,7 @@ namespace Docu.Tests.Documentation.DocumentModelGeneratorTests
             var model = new DocumentModel(RealParser, StubEventAggregator);
             var members = new IDocumentationMember[]
             {
-                Type<Second>(@"<member name=""T:Example.Second"">
-                  <example>
-                    void Something()
-                    {
-                      return;
-                    }
-                  </example>
-                </member>")
+                Type<Second>("<member name=\"T:Example.Second\"><example>\r\n                    void Something()\r\n                    {\r\n                      return;\r\n                    }\r\n                  </example></member>")
             };
             var namespaces = model.Create(members);
             var type = namespaces.Single().Classes.Single();
